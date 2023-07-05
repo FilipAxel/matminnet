c0a0225f61076b1a27375e32fc205e01cca1afa8
FilipAxel <filip_j97@hotmail.com>
2023-07-05 15:39:43 +0200

    add dialog for catalog

---
+++ src/components/create-catalog-dialog.tsx
@@ -0,0 +1,159 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+/* eslint-disable react/jsx-no-undef */
+import {
+  Button,
+  Checkbox,
+  Grid,
+  Input,
+  Modal,
+  Row,
+  Spacer,
+  Text,
+  useInput,
+} from "@nextui-org/react";
+import { useMemo } from "react";
+import { type SubmitHandler, useForm, Controller } from "react-hook-form";
+import { api } from "~/utils/api";
+
+interface IFormInput {
+  name: string;
+  type: string;
+}
+
+interface CreateCatalogDialogProps {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const CreateCatalogDialog: React.FC<CreateCatalogDialogProps> = ({
+  isOpen,
+  setIsOpen,
+}) => {
+  const closeHandler = () => {
+    setIsOpen(false);
+    reset();
+  };
+
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      type: "",
+    },
+  });
+
+  const { isLoading, mutate: createCatalog } =
+    api.catalog.createCatalog.useMutation({
+      onSuccess() {
+        console.log("success");
+      },
+    });
+
+  const onSubmit: SubmitHandler<IFormInput> = (FormdData) => {
+    setIsOpen(false);
+    createCatalog(FormdData);
+  };
+
+  console.log(errors);
+  return (
+    <div>
+      <Modal
+        closeButton
+        aria-labelledby="modal-title"
+        open={isOpen}
+        onClose={closeHandler}
+      >
+        <Modal.Header>
+          <Text id="modal-title">Create Catalog</Text>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+              name="name"
+              control={control}
+              rules={{
+                required: true,
+                maxLength: 20,
+                minLength: 3,
+              }}
+              render={({ field }) => (
+                <Input
+                  clearable
+                  bordered
+                  helperText={
+                    errors?.name?.type === "required"
+                      ? "Input is required"
+                      : "" || errors?.name?.type === "maxLength"
+                      ? "name must not exceed 100 characters"
+                      : ""
+                  }
+                  helperColor={
+                    errors.name?.type === "required"
+                      ? "error"
+                      : "primary" || errors?.name?.type === "maxLength"
+                      ? "error"
+                      : "primary"
+                  }
+                  color={errors.name?.type === "required" ? "error" : "primary"}
+                  aria-label={field.name}
+                  fullWidth
+                  placeholder={field.name}
+                  {...field}
+                  size="lg"
+                />
+              )}
+            />
+            <Spacer y={1.6} />
+            {/*       <Controller
+              name="type"
+              control={control}
+              rules={{
+                required: true,
+                maxLength: 20,
+                minLength: 3,
+              }}
+              render={({ field }) => (
+                <Input
+                  clearable
+                  bordered
+                  fullWidth
+                  aria-label={field.name}
+                  placeholder={field.name}
+                  {...field}
+                  color="primary"
+                  size="lg"
+                />
+              )}
+            />
+            <Spacer y={1.6} /> */}
+            <Grid.Container gap={2} justify="flex-end">
+              <Grid>
+                <Button
+                  type="button"
+                  onClick={() => reset()}
+                  auto
+                  flat
+                  color="primary"
+                >
+                  Clear
+                </Button>
+              </Grid>
+
+              <Grid>
+                <Button type="submit" auto flat color="primary">
+                  Create
+                </Button>
+              </Grid>
+            </Grid.Container>
+          </form>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+};
+
+export default CreateCatalogDialog;
---
+++ src/components/dialog/edit-action-dialog.tsx
@@ -0,0 +1,113 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { Input, Modal, Row, Text, Button, Spacer } from "@nextui-org/react";
+import { Controller, type SubmitHandler, useForm } from "react-hook-form";
+import { api } from "~/utils/api";
+
+interface editActionDialogProps {
+  id: string;
+  name: string;
+  isEditActionOpen: boolean;
+  setEditActionOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+interface IFormInput {
+  name: string;
+}
+
+const EditActionDialog: React.FC<editActionDialogProps> = ({
+  id,
+  name,
+  isEditActionOpen,
+  setEditActionOpen,
+}) => {
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: name,
+    },
+  });
+
+  const { mutate: updateCatalog } = api.catalog.updateCatalog.useMutation({
+    onSuccess() {
+      setEditActionOpen(false);
+    },
+  });
+
+  const onSubmit: SubmitHandler<IFormInput> = (FormdData) =>
+    updateCatalog({ id: id, name: FormdData.name });
+
+  return (
+    <div>
+      <Modal
+        closeButton
+        aria-labelledby="modal-title"
+        open={isEditActionOpen}
+        onClose={() => setEditActionOpen(false)}
+      >
+        <Modal.Header>
+          <Text id="modal-title" size={18}>
+            Edit {name}
+          </Text>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+              name="name"
+              control={control}
+              rules={{
+                required: true,
+                maxLength: 20,
+                minLength: 3,
+              }}
+              render={({ field }) => (
+                <Input
+                  aria-label={field.name}
+                  helperText={
+                    errors?.name?.type === "required"
+                      ? "Input is required"
+                      : "" || errors?.name?.type === "maxLength"
+                      ? "name must not exceed 100 characters"
+                      : ""
+                  }
+                  helperColor={
+                    errors.name?.type === "required"
+                      ? "error"
+                      : "primary" || errors?.name?.type === "maxLength"
+                      ? "error"
+                      : "primary"
+                  }
+                  color={errors.name?.type === "required" ? "error" : "primary"}
+                  clearable
+                  bordered
+                  fullWidth
+                  placeholder={field.name}
+                  {...field}
+                  size="lg"
+                />
+              )}
+            />
+            <Spacer y={1.6} />
+            <Modal.Footer>
+              <Button
+                auto
+                flat
+                color="error"
+                onPress={() => setEditActionOpen(false)}
+              >
+                Close
+              </Button>
+              <Button color="primary" type="submit" flat auto>
+                Save
+              </Button>
+            </Modal.Footer>
+          </form>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+};
+
+export default EditActionDialog;
