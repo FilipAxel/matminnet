1c45a15c248ca8c246c103860127594f2b16ef79
FilipAxel <filip_j97@hotmail.com>
2023-07-07 23:15:20 +0200

    convert application to PWA

---
+++ next.config.cjs
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+export default withPWA({
+  // next.js config
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+});
