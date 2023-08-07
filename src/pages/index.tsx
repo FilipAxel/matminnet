f2580fbdc441539be8c81b4dc3e6cab71e99eaca
FilipAxel <filip_j97@hotmail.com>
2023-08-07 19:04:04 +0200

    update schema and fix login redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,20 @@
 import { Grid, Loading } from "@nextui-org/react";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import CatalogCard from "~/components/catalog/catalog-card";
 import CreateCatalog from "~/components/catalog/create-catalog";
-import LoginActionDialog from "~/components/dialog/login-action-dialog";
 import { api } from "~/utils/api";
 
 export default function Home() {
   const { status, data: session } = useSession();
-
+  const router = useRouter();
   const { data: catalogs, isLoading } = api.catalog.getCatalogs.useQuery(
     undefined, // no input
     { enabled: session?.user !== undefined }
   );
 
   if (status === "unauthenticated" && !session) {
-    return <LoginActionDialog pageName={"catalog"} />;
+    void router.push("/public");
   }
 
   if (status === "loading" || isLoading) {
@@ -25,23 +25,25 @@ export default function Home() {
     );
   }
 
-  return (
-    <>
-      <div className="flex flex-col items-center justify-center gap-4">
-        <div className="flex w-full justify-center">
-          <Grid.Container
-            className="mx-auto flex w-full max-w-[1200px] justify-start"
-            gap={2}
-          >
-            <Grid>
-              <CreateCatalog />
-            </Grid>
-            {catalogs?.map((catalog) => (
-              <CatalogCard key={catalog.id} catalog={catalog} />
-            ))}
-          </Grid.Container>
+  if (status === "authenticated" || !isLoading) {
+    return (
+      <>
+        <div className="flex flex-col items-center justify-center gap-4">
+          <div className="flex w-full justify-center">
+            <Grid.Container
+              className="mx-auto flex w-full max-w-[1200px] justify-start"
+              gap={2}
+            >
+              <Grid>
+                <CreateCatalog />
+              </Grid>
+              {catalogs?.map((catalog) => (
+                <CatalogCard key={catalog.id} catalog={catalog} />
+              ))}
+            </Grid.Container>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
