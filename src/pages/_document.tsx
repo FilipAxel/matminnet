663511580df2428ee5c72ee5c301f22109ea7cc4
FilipAxel <filip_j97@hotmail.com>
2023-08-11 14:33:17 +0200

    test zoom ability on mobile

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -30,7 +30,7 @@ class MyDocument extends Document {
           <meta name="author" content="Filip Johansson" />
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1.0"
+            content="width=device-width, initial-scale=1, maximum-scale=1"
           />
           <meta
             name="description"
