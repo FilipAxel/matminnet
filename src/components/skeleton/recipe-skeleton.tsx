c43ddd60d8728025fcac5cfbf3bfb0f1e7211931
FilipAxel <filip_j97@hotmail.com>
2023-10-23 09:27:46 +0200

    update recipe skeletion to work with the new design

--- src/components/skeleton/recipe-skeleton.tsx
+++ src/components/skeleton/recipe-skeleton.tsx
@@ -4,7 +4,14 @@ import { Card, CardBody } from "@nextui-org/react";
 const SkeletonRecipe = () => {
   return (
     <div className="mb-10 max-w-5xl">
-      <div className="m-auto mt-12 h-[380px] w-[250px] animate-pulse rounded-[15px] bg-gray-300"></div>
+      <h1 className="m-auto mx-5 mt-5 max-w-[550px] animate-pulse bg-gray-300 text-left font-semibold">
+        &nbsp;
+      </h1>
+      <div className="mt-5 flex  items-baseline gap-3 px-5">
+        <span className="w-[60px] animate-pulse bg-gray-300 px-4">&nbsp;</span>
+        <span className="w-[60px] animate-pulse bg-gray-300 px-4">&nbsp;</span>
+      </div>
+      <div className="m-auto mt-6 h-[380px] max-w-[400px] animate-pulse rounded-[15px] bg-gray-300 px-2 first-letter:w-full"></div>
       <div className=" flex justify-center gap-4">
         <div className="mt-5 grid cursor-pointer rounded-[10px]">
           <div className="h-[100px] w-[100px] animate-pulse bg-gray-300"></div>
@@ -16,9 +23,6 @@ const SkeletonRecipe = () => {
           <div className="h-[100px] w-[100px] animate-pulse bg-gray-300"></div>
         </div>
       </div>
-      <h1 className="font-size[30px] m-auto mt-5 w-[80%] animate-pulse bg-gray-300 text-center font-semibold">
-        &nbsp;
-      </h1>
 
       <div className="mt-5 flex flex-row items-center justify-between">
         <h2 className="ml-4 text-[25px]  font-semibold">Ingredients</h2>
@@ -27,28 +31,28 @@ const SkeletonRecipe = () => {
           <span className="animate-pulse bg-gray-300 px-4">&nbsp;</span>
         </h3>
       </div>
-      <div className="container mt-4 grid justify-center gap-4">
-        <div className="grid">
-          <Card className="max-w-[400px]">
+      <div className="container mt-4 justify-center px-4">
+        <div>
+          <Card className="my-4 w-full">
             <CardBody className="flex animate-pulse flex-row items-center  justify-between">
-              <h2 className="w-[250px] animate-pulse bg-gray-400 py-4 font-bold"></h2>
+              <h2 className="w-3/4 animate-pulse bg-gray-400 py-4 font-bold"></h2>
               <h3 className="animate-pulse bg-gray-400 p-4 text-[15px]"></h3>
             </CardBody>
           </Card>
         </div>
-        <div className="grid">
-          <Card className="max-w-[400px]">
+        <div>
+          <Card className="my-4 w-full">
             <CardBody className="flex animate-pulse flex-row items-center  justify-between">
-              <h2 className="w-[250px] animate-pulse bg-gray-400 py-4 font-bold"></h2>
+              <h2 className="w-3/4 animate-pulse bg-gray-400 py-4 font-bold"></h2>
               <h3 className="animate-pulse bg-gray-400 p-4 text-[15px]"></h3>
             </CardBody>
           </Card>
         </div>
-        <div className="grid">
-          <Card className="max-w-[400px]">
+        <div>
+          <Card className="my-4 w-full">
             <CardBody className="flex animate-pulse flex-row items-center  justify-between">
-              <h2 className="font-bald w-[250px] animate-pulse bg-gray-400 py-4"></h2>
-              <h3 className="animate-pulse bg-gray-400 p-4 font-[15px]"></h3>
+              <h2 className="w-3/4 animate-pulse bg-gray-400 py-4 font-bold"></h2>
+              <h3 className="animate-pulse bg-gray-400 p-4 text-[15px]"></h3>
             </CardBody>
           </Card>
         </div>
